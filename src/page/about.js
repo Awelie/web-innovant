f3ec661cfeb5a5a2a8ebfe76f02b77c03da0ead7
amjac <amelie.jacobguizon@gmail.com>
2022-05-04 13:20:33 +0200

    test

--- src/page/about.js
+++ src/page/about.js
@@ -10,7 +10,26 @@ export class About extends React.Component {
                     color: "white"
                 }}
                 >
-			        <Link to="/logout">Déconnection</Link>
+                    <h1>Profile</h1>
+                    <div className="displayData">
+                        <h2>John Keller</h2>
+                        <h3>Homme 35kg 185cm</h3>
+                        <button>Modifier</button>
+                    </div>
+                    <div>
+                        <h3>Dark Mode</h3>
+                    </div>
+                    <div>
+                        <h3>Notifications</h3>
+                        <h3>Privacy</h3>
+                        <h3>Security</h3>
+                    </div>
+                    <div>
+                        <h3>Account</h3>
+                        <h3>Aid</h3>
+                        <h3>About</h3>
+                    </div>
+			        <Link to="/logout">Deconnection</Link>
                 </div>
                 <Navbar />
             </>
